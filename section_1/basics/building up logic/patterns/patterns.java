8268166b3cdb5101775276f7390abfbfb425c531
shaik-adnan1 <yousufwrk0@gmail.com>
2024-03-07 23:42:40 +0530

    more patterns java

--- section_1/basics/building up logic/patterns/patterns.java
+++ section_1/basics/building up logic/patterns/patterns.java
@@ -8,9 +8,7 @@
  * (4) Observe symmetry
  */
 
-
 public class patterns {
-    
 
     // PATTERN - 1
     /*
@@ -20,16 +18,15 @@ public class patterns {
      * *****
      * *****
      */
-    
+
     public static void printPattern1(int n) {
 
-
-        for(int i = 0; i < n; i++) {
-            for(int j = 0; j < n; j++) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
                 System.out.print("* ");
             }
             System.out.println(" ");
-        }       
+        }
     }
 
     // PATTERN - 2
@@ -42,13 +39,12 @@ public class patterns {
      */
 
     public static void printPattern2(int n) {
-       
 
         String row = "";
-         for(int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++) {
             row += "* ";
-            System.out.println(row);    
-         }
+            System.out.println(row);
+        }
     }
 
     // PATTERN - 3
@@ -60,43 +56,58 @@ public class patterns {
      * 12345
      */
 
-     public static void printPattern3(int n) {
+    public static void printPattern3(int n) {
 
         String row = "1";
         int sum = 1;
         for (int i = 0; i < n; i++) {
             System.out.println(row);
             sum++;
-            row += sum+" ";
+            row += sum + " ";
         }
     }
 
-    public static void nTriangle(int n) {
-        // Write your code here
-        String extra = "";
-        int row2 = 1;
-        int sum2 = 1;
-        for (int i = 0; i < n; i++) {
-            System.out.println(row2);
-            sum2++;
-            row2++;
-            row2+=sum2;
-        }
+    // PATTERN - 3
+    /**
+     * 1
+     * 2 2
+     * 3 3 3
+     * 
+     * @param n
+     */
 
-    
-    }
-    
     public static void nTriangle2(int n) {
         // Write your code here
-        int number = 1;
-        String row = "";
-
         for (int i = 1; i <= n; i++) {
-            row += number + " ";
+            String row = "";
+            for (int j = 1; j <= i; j++) {
+                row += i + " ";
+
+            }
             System.out.println(row);
-            number++;
         }
     }
+
+    // PATTERN - 3
+    /**
+     * * * *
+     * * *
+     * *
+     * 
+     * @param n
+     */
+
+    public static void nTriangle3(int n) {
+        // Write your code here
+        for (int i = n; i > 0; i--) {
+            String row = "";
+            for (int j = 0; j < i; j++) {
+                row += "* ";
+            }
+            System.out.println(row);
+        }
+    }
+
 
 
     public static void main(String[] args) {
@@ -104,6 +115,7 @@ public class patterns {
         printPattern2(5);
         printPattern3(5);
         nTriangle2(3);
+        nTriangle3(3);
     }
 
 }
